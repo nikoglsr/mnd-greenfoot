d3b0efb5f984a7a45bb166d8902f8f0cc9460748
Nikolaos Glaser <nikolaos.glaser@tms-hl.org>
2021-02-19 09:21:38 +0100

    Greenfoot Programm

---
+++ mnd-Das_Projekt/test_enemy.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class test_enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test_enemy extends player_and_enemy
+{
+    /**
+     * Act - do whatever the test_enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ mnd-Das_Projekt/test_schuss.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class test_schuss here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test_schuss extends Actor
+{
+    /**
+     * Act - do whatever the test_schuss wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
